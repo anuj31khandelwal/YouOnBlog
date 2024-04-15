cb1800de849d86a6c5024cb8063104c48ead2b1c
shreem-123 <asatishreem@gmail.com>
2024-04-16 00:38:50 +0530

    New project up now!

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import React,{useEffect,useState} from 'react';
+import { Layout } from '../components/Layout';
+import 'tailwindcss/tailwind.css';
+import "@/styles/globals.scss";
+
+export default function App({ Component, pageProps }) {
+  return 
+  <Layout>
+  <Component {...pageProps} />;
+  </Layout>
+}
